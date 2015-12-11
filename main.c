589012d40d541f2cff73bd394c483a8dbc9c4d4f
Kingpin007 <anirudhkanabar@gmail.com>
2015-12-11 22:37:46 +0530

    Quick Sort - 1.3
    Implemented randomized quick sort, now the average time
    complexity has been improved. The space complexity needs
    to be worked on. At present after calculations I have
    realized it is Theta(n) for now dues to my not clearing
    the data so I will be working on that in the next commit.
    The release is coming soon.

--- main.c
+++ main.c
@@ -1,9 +1,17 @@
+/*
+Author : Anirudh Kanabar
+Date of Completion : 11/12/2015
+Time : 10:36PM IST
+Subject: Randomized quickSort in C
+Time Complexity : Theta(nlogn)
+Space Complexity : Theta(n) - For now
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
 void quickSort(int a[], int start,int end);
 int partition(int a[],int start,int end);
-//void randomizedPartiton(int *a,int start,int end);
+int randomizedPartiton(int a[],int start,int end);
 int main()
 {
   int a[10] = {9,7,5,3,1,8,6,2,0,4};
@@ -34,12 +42,20 @@ int partition(int a[],int start,int end)
   a[end] = temp;
   return partitionIndex;
 }
-
+int randomizedPartiton(int a[],int start,int end)
+{
+  int r = (rand() % (end+1-start))+start;
+  int temp = a[r];
+  a[r] = a[end];
+  a[end] = temp;
+  temp = partition(a,start,end);
+  return temp;
+}
 void quickSort(int a[],int start, int end)
 {
   if(start < end)
   {
-    int partitionIndex = partition(a,start,end);
+    int partitionIndex = randomizedPartiton(a,start,end);
     quickSort(a,start,partitionIndex-1);
     quickSort(a,partitionIndex+1,end);
   }
